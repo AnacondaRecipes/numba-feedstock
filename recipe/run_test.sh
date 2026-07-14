831b8bc4e07fa283273f797108441b4e1178a814
Andrii-Osipov <aosipov@anaconda.com>
2026-07-14 01:46:45 -0700

    numba 0.66.0

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,6 @@ set -e
 
 export NUMBA_DEVELOPER_MODE=1
 export NUMBA_DISABLE_ERROR_MESSAGE_HIGHLIGHTING=1
-export NUMBA_CAPTURED_ERRORS="new_style"
 export PYTHONFAULTHANDLER=1
 
 unamestr=`uname`
@@ -17,28 +16,10 @@ else
   echo Error
 fi
 
-# limit CPUs in use on PPC64LE and AARCH64, fork() issues
-# occur on high core count systems
-archstr=`uname -m`
-if [[ "$archstr" == 'ppc64le' ]]; then
-    TEST_NPROCS=1
-elif [[ "$archstr" == 'aarch64' ]]; then
-    TEST_NPROCS=4
-else
-    TEST_NPROCS=${CPU_COUNT}
-fi
+TEST_NPROCS=${CPU_COUNT}
 
-# Disable NumPy dispatching to AVX512_SKX if the CPU supports it AND it's
-# in NumPy's dispatch list. This avoids low accuracy SVML libm replacements.
-# This is needed because only dispatched features can be disabled via NPY_DISABLE_CPU_FEATURES.
-_NPY_CMD='from numba.misc import numba_sysinfo; si=numba_sysinfo.get_sysinfo();\
-          print(si["NumPy AVX512_SKX detected"] and "AVX512_SKX" in si.get("NumPy Supported SIMD dispatch", ()))'
-DISABLE_AVX512_SKX=$(python -c "$_NPY_CMD")
-echo "Disable AVX512_SKX: $DISABLE_AVX512_SKX"
-
-if [[ "$DISABLE_AVX512_SKX" == "True" ]]; then
-    export NPY_DISABLE_CPU_FEATURES="AVX512_SKX"
-fi
+# Validate Numba dependencies
+python -m pip check
 
 # Check Numba executables are there
 numba -h
@@ -49,8 +30,25 @@ numba -s
 # Check test discovery works
 python -m numba.tests.test_runtests
 
-echo 'Running all the tests except long_running'
-echo "Running: $SEGVCATCH python -m numba.runtests -b -m $TEST_NPROCS -- $TESTS_TO_RUN"
-$SEGVCATCH python -m numba.runtests -b --exclude-tags='long_running' -m $TEST_NPROCS -- $TESTS_TO_RUN
+if [[ "$build_platform" != "$target_platform" ]]; then
+	echo "Skipping numba test suite on $archstr because $build_platform != $target_platform"
+else
+	echo "Running all the tests except long_running on '$target_platform'"
 
-pip check
+    # Disable NumPy dispatching to AVX512_SKX feature extensions if the chip is
+    # reported to support the feature and NumPy >= 1.22 as this results in the use
+    # of low accuracy SVML libm replacements in ufunc loops.
+    _NPY_CMD='from numba.misc import numba_sysinfo;\
+              sysinfo=numba_sysinfo.get_sysinfo();\
+              print(sysinfo.get("NumPy AVX512_SKX detected", False) and
+                    sysinfo["NumPy Version"]>="1.22")'
+    NUMPY_DETECTS_AVX512_SKX_NP_GT_122=$(python -c "$_NPY_CMD")
+    echo "NumPy >= 1.22 with AVX512_SKX detected: $NUMPY_DETECTS_AVX512_SKX_NP_GT_122"
+
+    if [[ "$NUMPY_DETECTS_AVX512_SKX_NP_GT_122" == "True" ]]; then
+        export NPY_DISABLE_CPU_FEATURES="AVX512_SKX"
+    fi
+
+	echo "Running: $SEGVCATCH python -m numba.runtests -b -m $TEST_NPROCS -- $TESTS_TO_RUN"
+    $SEGVCATCH python -m numba.runtests -b --exclude-tags='long_running' -m $TEST_NPROCS -- $TESTS_TO_RUN
+fi
